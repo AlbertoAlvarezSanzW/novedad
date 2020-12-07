0fd2c41691f6e186bb510159c2ac08ee65bc57e0
Alberto <alberto.alvarez.sanz@gmail.com>
2020-12-07 18:52:16 +0100

    commit

--- app/src/main/java/com/example/metodos/MainActivity.kt
+++ app/src/main/java/com/example/metodos/MainActivity.kt
@@ -7,18 +7,29 @@ import android.os.Bundle
 import android.widget.*
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.metodos.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val boton = findViewById<Button>(R.id.boton2)
+        val text2 = findViewById<EditText>(R.id.text2)
+        boton.setOnClickListener {
+            Toast.makeText(this@MainActivity, text2.text.toString(), Toast.LENGTH_LONG).show()
+        }
+
 
         binding.boton1.setOnClickListener {
             Toast.makeText(this@MainActivity, binding.text.text, Toast.LENGTH_LONG).show()
 
+        }
+
+        boton3.setOnClickListener {
+            Toast.makeText(this@MainActivity, text3.text, Toast.LENGTH_LONG).show()
         }
     }
 }
